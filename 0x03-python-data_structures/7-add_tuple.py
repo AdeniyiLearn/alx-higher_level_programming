ef12a412fd1c8b33fb26ddfcea497f36e817a4a1
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-08 06:29:40 +0100

    7th task redone

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,15 +2,32 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0 and len(tuple_b) != 0:
-            return tuple_b
-        tuple_c = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_b) < 2:
-        if len(tuple_b) == 0 and len(tuple_a) != 0:
-            return tuple_a
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    a = 0
+    b = 0
+    list_1 = []
+    list_2 = []
+    dull = 0
+    if len(tuple_a) >= 2:
+        for i in range(2):
+            list_1.append(tuple_a[i])
+    elif len(tuple_a) == 1:
+        list_1.append(tuple_a)
+        list_1.append(dull)
     else:
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        list_1.append(dull)
+        list_1.append(dull)
 
-    return tuple_c
+    if len(tuple_b) >= 2:
+        for i in range(2):
+            list_2.append(tuple_b[i])
+    elif len(tuple_b) == 1:
+        list_2.append(tuple_b[0])
+        list_2.append(dull)
+    else:
+        list_2.append(dull)
+        list_2.append(dull)
+
+    tuple_c = list(zip(list_1, list_2))
+    a = sum(tuple_c[0])
+    b = sum(tuple_c[1])
+    return a, b
