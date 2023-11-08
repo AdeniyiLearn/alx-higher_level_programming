049340ec08a551b5f0b1c2eeb4bfde51440ee9f0
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-08 02:53:14 +0100

    code redone

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
     count = len(my_list)
     index = -1
-
-    if my_list == None:
+    if my_list is None:
         return []
     else:
         for num in my_list:
