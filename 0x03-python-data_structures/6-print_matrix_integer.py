2a354cde89754dd34fd50a076824a91da20ab240
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-08 03:36:46 +0100

    add 2nd if

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        return []
+        return
     for row in matrix:
         for item in row:
-            print("{:d} ".format(item), end='')
+            print("{:d}".format(item), end='')
+            if item != row[-1]:
+                print(" ", end='')
         print()
