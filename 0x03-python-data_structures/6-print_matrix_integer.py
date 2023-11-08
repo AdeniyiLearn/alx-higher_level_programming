800b2b8020f50b56d08a227833a6b5d4fed3af25
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-08 03:14:09 +0100

    task 6 redone

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    count = 0
-    for i in matrix:
-        if matrix:
-            for j in i:
-                print('{:d}'.format(j), end='')
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item), end='')
         print()
