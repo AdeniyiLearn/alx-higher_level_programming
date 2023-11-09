2bb4b695301ebb9bc52258e65ce6737171c250b7
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-09 04:24:46 +0100

    ff

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    """ computes the square value of all matrix integer.
+    '''computes the square value of all matrix integer.
 
     args:
         matrix: a list of 3 lists(2 dimensional array.
     Returns: new list modified as squares of previous data).
-    """
+    '''
     new_matrix = [[element**2 for element in block] for block in matrix]
     return(new_matrix)
