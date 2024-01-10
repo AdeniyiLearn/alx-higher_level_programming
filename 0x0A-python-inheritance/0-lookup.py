b9ed840b9aab189d97567a57ee18d809d37515d9
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-01-11 00:47:16 +0100

    add module doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""This module defines a function named lookup, it returns the
+list of the attributes and methods of an object
+"""
+
 
 def lookup(obj):
     """Function returns a list of attributes and method of an object
