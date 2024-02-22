866299049087a366d01d6e5633e4b6883473bb55
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-02-22 14:35:11 +0100

    task 0 redo

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -28,7 +28,7 @@ def add_integer(a, b=98):
     '''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(a, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     if type(a) == float:
         a = int(a)
