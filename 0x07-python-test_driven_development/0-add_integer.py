30a7b8b12a264160836dbc1aba6594e55616d23c
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-02-22 15:03:04 +0100

    task 0 redo

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,14 +26,14 @@ def add_integer(a, b=98):
         b: second integer
     Return: the addition of a and b
     '''
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
     if type(a) == float:
         a = int(a)
     if type(b) == float:
         b = int(b)
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     return (a + b)
 
 
