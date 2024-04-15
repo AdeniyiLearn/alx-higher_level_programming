16c54bba8de3d43e67012d678de150b446deebf0
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-15 22:30:46 +0100

    requirements fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+
 def lookup(obj):
-    """ Function returns the list of available attributes and methods of an object
+    """ Function returns the list of available attributes
+    and methods of an object
 
     args::
         obj : a python object
 
     Returnsi: a list object
     """
+
     return help(obj)
