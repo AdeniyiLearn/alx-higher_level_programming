3629890421376873198f763182ea6ba20f2356ad
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-18 05:27:16 +0100

    white taken

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,31 +5,18 @@
 
 class BaseGeometry:
     """A class that has only area() public method.
-
-        Attributes
-        ----------
-        NONE
-
-        Methods
-        -------
-
-        NONE
-
         Public Methods
         -------------
             area(self)
             raise Exception "area() is not implemented"
-
      """
 
     def area(self):
-        '''Returns the area of rectangle '''
-        raise Exception ("area() is not implemented")
+        """Raises Exception with below message..."""
+        raise Exception("area() is not implemented")
 
 
 if __name__ == "__main__":
-    BaseGeometry = __import__('6-base_geometry').BaseGeometry
-
     bg = BaseGeometry()
 
     try:
