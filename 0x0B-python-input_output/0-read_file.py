56677fd45069e4fbb81dd29f007b72fd041111cc
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-18 01:46:54 +0100

    0-read_file init

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" This module creates a function read_file(filename="")
+    that reads a text file (UTF-8) and prints to stdout:
+"""
+
+
+def read_file(filename=""):
+    """ Function read a text file and prints
+    to stdout
+
+    args:
+        filename : a python object
+
+    Returns: None
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
+
+
+if __name__ == "__main__":
+    read_file = __import__('0-read_file').read_file
+
+    read_file("my_file_0.txt")
