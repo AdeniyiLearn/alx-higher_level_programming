cdf0f94cf6d1c0b2513c49fa24062cc4f7da34bc
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-26 17:03:51 +0100

    try

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -160,5 +160,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns informal info representation of object"""
-        return ("[Rectangle] ({}) {}/{} - {}/{} ".format(
-            self.id, self.__x, self.__y, self.__width, self.__height))
+        return ("[Rectangle] ({}) {}/{} - {}/{} ".format(self.__class__
+            .__name__, self.__x, self.__y, self.__width, self.__height))
