049cde4a5fb176f7bf4be3e4369e2fcffa3b10d7
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-27 01:48:50 +0100

    update method init in square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,16 +18,59 @@ class Square(Rectangle):
             y: y coordinate
             id: identifier
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be > 0")
 
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """returns the attribute size in width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets value for height & width
+        Aegs:
+            value: object width in int
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Method assigms an argument to each attribut
+
+        args:
+            *args: multiple non keyword arguments
+        """
+        if args and len(args) != 0:
+            for arg in args:
+                if len(args) == 1:
+                    self.id = args[0]
+                elif len(args) == 2:
+                    self.id = args[0]
+                    self.size = args[1]
+                elif len(args) == 3:
+                    self.id = args[0]
+                    self.size = args[1]
+                    self.x = args[2]
+                elif len(args) == 4:
+                    self.id = args[0]
+                    self.size = args[1]
+                    self.x = args[2]
+                    self.y = args[3]
+        elif kwargs and len(kwargs) != 0:
+            for key, arg in kwargs.items():
+                if key == 'id':
+                    self.id = arg
+                elif key == 'size':
+                    self.size = arg
+                elif key == 'x':
+                    self.x = arg
+                elif key == 'y':
+                    self.y = arg
 
     def __str__(self):
         """informal string representation of the object"""
         return ("[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__,
             self.id, self.x, self.y,
-            self.height))
+            self.size))
